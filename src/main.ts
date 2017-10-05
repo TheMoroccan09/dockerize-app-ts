b0b7a67b49be49985b87a867b4e6232a3e877d29
Mohimi Othmane <mohimi.othmane@gmail.com>
2017-10-05 12:38:49 +0100

    add main.ts to test compilation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 /**
  * Created by themoroccan09 on 05/10/17.
  */
+class Application {
+
+    sayHello() : void{
+        console.log('Hello World!')
+    }
+}
+
+let app = new Application();
+app.sayHello();
